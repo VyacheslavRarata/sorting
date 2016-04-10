763c66c4ddca4e8a05b36e3e5bb41fdb5543fc7c
VyacheslavRarata <cayneir@gmail.com>
2016-04-10 13:19:11 +0300

    add SelectSort to App; create methods to all selects inside App

--- src/org/sorting/run/App.java
+++ src/org/sorting/run/App.java
@@ -3,37 +3,16 @@ package org.sorting.run;
 import org.sorting.api.Sort;
 import org.sorting.impl.BubbleSort;
 import org.sorting.impl.InsertSort;
+import org.sorting.impl.SelectSort;
 
 public class App {
 
+	public static Sort sorting;
+	public static int[] inputArray;
+	
 	public static void main(String[] args) {
-		int[] inputArray = { 3, 0, 1, 8, 7, 2, 5, 4, 9, 6 };
 		System.out.println("Welocme to Sorting World!");
-		Sort sorting = new BubbleSort();
-		inputArray = sorting.sort(inputArray);
-		System.out.print(sorting.getClass().getSimpleName() + " = ");
-		showArray(inputArray);
-		inputArray = new int[] { 0 };
-		inputArray = sorting.sort(inputArray);
-		System.out.print(sorting.getClass().getSimpleName() + " = ");
-		showArray(inputArray);
-		inputArray = new int[] { -20, 23, 51, -2 };
-		inputArray = sorting.sort(inputArray);
-		System.out.print(sorting.getClass().getSimpleName() + " = ");
-		showArray(inputArray);
-		sorting = new InsertSort();
-		inputArray = sorting.sort(inputArray);
-		System.out.print(sorting.getClass().getSimpleName() + " = ");
-		showArray(inputArray);
-		inputArray = new int[] { 0 };
-		inputArray = sorting.sort(inputArray);
-		System.out.print(sorting.getClass().getSimpleName() + " = ");
-		showArray(inputArray);
-		inputArray = new int[] { -20, 23, 51, -2 };
-		inputArray = sorting.sort(inputArray);
-		System.out.print(sorting.getClass().getSimpleName() + " = ");
-		showArray(inputArray);
-
+		runSorting();
 	}
 
 	public static void showArray(int[] inputArray) {
@@ -42,5 +21,56 @@ public class App {
 			System.out.print(element + " ");
 		}
 		System.out.println("]");
+	}
+	public static void runSorting(){
+		runBubbleSort();
+		runInsertSort();
+		runSelectSort();
+	}
+	private static void runBubbleSort(){
+		inputArray = new int[] { 3, 0, 1, 8, 7, 2, 5, 4, 9, 6 };
+		sorting = new BubbleSort();
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		inputArray = new int[] { 0 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		inputArray = new int[] { -20, 23, 51, -2 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		
+	}
+	private static void runInsertSort(){
+		sorting = new InsertSort();
+		inputArray = new int[]{ 3, 0, 1, 8, 7, 2, 5, 4, 9, 6 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		inputArray = new int[] { 0 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		inputArray = new int[] { -20, 23, 51, -2 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+	}
+	private static void runSelectSort(){
+		sorting = new SelectSort();
+		inputArray = new int[]{ 3, 0, 1, 8, 7, 2, 5, 4, 9, 6 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		inputArray = new int[] { 0 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
+		inputArray = new int[] { -20, 23, 51, -2 };
+		inputArray = sorting.sort(inputArray);
+		System.out.print(sorting.getClass().getSimpleName() + " = ");
+		showArray(inputArray);
 	}
 }

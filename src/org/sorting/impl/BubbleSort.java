ba32efeef6b7560544eebf4f7f9fc2c2af371f66
VyacheslavRarata <cayneir@gmail.com>
2016-04-10 09:05:22 +0300

    add impl for Sort as BuubleSort

---
+++ src/org/sorting/impl/BubbleSort.java
@@ -0,0 +1,22 @@
+package org.sorting.impl;
+
+import org.sorting.api.Sort;
+
+public class BubbleSort implements Sort {
+
+	@Override
+	public int[] sort(int[] inputArray) {
+		for (int i = 0; i < inputArray.length; i++) {
+			for (int j = 0, k = 1; j < inputArray.length - i - 1; j++, k++) {
+				if (inputArray[k] < inputArray[j]) {
+					// changing elements
+					inputArray[j] += inputArray[k];
+					inputArray[k] = inputArray[j] - inputArray[k];
+					inputArray[j] -= inputArray[k];
+				}
+			}
+		}
+		return inputArray;
+	}
+
+}

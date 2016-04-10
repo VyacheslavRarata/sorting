76693ded7542385bbd0f573c7355e4b56b4ca156
VyacheslavRarata <cayneir@gmail.com>
2016-04-10 12:11:20 +0300

    implement sort method in InsertSort.java

--- src/org/sorting/impl/BubbleSort.java
+++ src/org/sorting/impl/BubbleSort.java
@@ -18,5 +18,4 @@ public class BubbleSort implements Sort {
 		}
 		return inputArray;
 	}
-
 }
--- src/org/sorting/impl/InsertSort.java
+++ src/org/sorting/impl/InsertSort.java
@@ -1,5 +1,22 @@
 package org.sorting.impl;
 
-public class InsertSort {
+import org.sorting.api.Sort;
+
+public class InsertSort implements Sort {
+
+	@Override
+	public int[] sort(int[] inputArray) {
+		for (int i = 1; i < inputArray.length; i++) {
+			for (int j = i, k = i - 1; j > 0; j--, k--) {
+				if (inputArray[k] > inputArray[j]) {
+					// changing elements
+					inputArray[j] += inputArray[k];
+					inputArray[k] = inputArray[j] - inputArray[k];
+					inputArray[j] -= inputArray[k];
+				}
+			}
+		}
+		return inputArray;
+	}
 
 }

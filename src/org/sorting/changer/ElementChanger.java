8ce2d2e0b560d4a33bb3beef929d532eb3e1a0f8
VyacheslavRarata <cayneir@gmail.com>
2016-04-10 14:13:52 +0300

    ElementChanger.java: add condition for ArrayOut of boud

--- src/org/sorting/changer/ElementChanger.java
+++ src/org/sorting/changer/ElementChanger.java
@@ -2,10 +2,22 @@ package org.sorting.changer;
 
 public class ElementChanger {
 
-	public static int[] changeElements(int[] inputArray, int firstElementPosition, int secondElementPosition){
-		inputArray[firstElementPosition] += inputArray[secondElementPosition];
-		inputArray[secondElementPosition] = inputArray[firstElementPosition] - inputArray[secondElementPosition];
-		inputArray[firstElementPosition] -= inputArray[secondElementPosition];
+	public static int[] changeElements(int[] inputArray, int firstElementPosition, int secondElementPosition) {
+		if (checkConditions(inputArray, firstElementPosition, secondElementPosition)) {
+			inputArray[firstElementPosition] += inputArray[secondElementPosition];
+			inputArray[secondElementPosition] = inputArray[firstElementPosition] - inputArray[secondElementPosition];
+			inputArray[firstElementPosition] -= inputArray[secondElementPosition];
+		} else
+			throw new ArrayIndexOutOfBoundsException();
 		return inputArray;
+	}
+
+	private static boolean checkConditions(int[] inputArray, int firstElementPosition, int secondElementPosition) {
+		if (firstElementPosition >= inputArray.length || secondElementPosition >= inputArray.length)
+			return false;
+		else if (firstElementPosition < 0 || firstElementPosition < 0)
+			return false;
+		else
+			return true;
 	}
 }
